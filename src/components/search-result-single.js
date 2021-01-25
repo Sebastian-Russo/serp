0184dcf171e1c0efd017aa9702d77f368ddd5192
Sebastian Russo <russo.sebastian@gmail.com>
2021-01-25 17:50:33 -0500

    need to set up video player

--- src/components/search-result-single.js
+++ src/components/search-result-single.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 // import { Redirect } from 'react-router-dom';
 
 const SearchResultSingle = ({single}) => {
-  console.log(single)
+  console.log(single.uri)
+  
+  const video = single.uri 
+
   return ( 
      <div>
         <div className="container">
         <div>{single.name}</div>
         <div>{single.user.name}</div>
         <div>Views (check stats)</div>
-            <div>
-              <video width="320" height="240">
-                <source src={single.uri} type="" />
-              </video>
-            </div>
-            <div>Duration: {single.duration}</div>
-            <div>Location: {single.user.location}</div>
-            <div>Comments: {single.metadata.connections.comments.total}</div>
-            <div>Description: {single.description}</div>
-          </div>
+        <div>
+        <ReactPlayer url="https://vimeo.com/video/162413069" />
+            {/* <ReactPlayer url={`https://vimeo.com/${video}`} /> */}
+            {/* <video key={video}>
+              <source src={`https://vimeo.com/${video}`} />
+          </video> */}
+        </div>
+        <div>Duration: {single.duration}</div>
+        <div>Location: {single.user.location}</div>
+        <div>Comments: {single.metadata.connections.comments.total}</div>
+        <div>Description: {single.description}</div>
+        </div>
       </div>
   );
 }
